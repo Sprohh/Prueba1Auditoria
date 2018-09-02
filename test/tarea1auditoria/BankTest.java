be25e0cbc88d45b4104c7fec2ea586b2da65a5d1
Sergio Orellana <sergio.orellana@taisachile.cl>
2018-09-02 19:23:16 -0300

    Implementación de clases de testing para Account y Bank (Por programar)

---
+++ test/tarea1auditoria/BankTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea1auditoria;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author orell
+ */
+public class BankTest {
+    
+    public BankTest() {
+    }
+
+    //Prueba del método openAccount
+    @Test
+    public void testOpenAccount() {
+    }
+    //Prueba de excepción AccountExistsException en el método openAccount
+    @Test
+    public void testAccountAlreadyExist() {
+    }
+    //Prueba del método deposit
+    @Test
+    public void testDeposit() {
+    }
+    //Prueba de excepción AccountDoesNotExistException en el método deposit
+    @Test
+    public void testDepositToAccountThatDoesntExists() {
+    }
+    //Prueba del método withdraw
+    @Test
+    public void testWithdraw() {
+    }
+    //Prueba de excepción AccountDoesNotExistException en el método withdraw
+    @Test
+    public void testWithdrawToAccountThatDoesntExists() {
+    }
+    //Prueba del método getBalance
+    @Test
+    public void testGetBalance() {
+    }
+    //Prueba de excepción AccountDoesNotExistException en el método getBalance
+    @Test
+    public void testGetBalanceToAccountThatDoesntExists() {
+    }
+    //Prueba del método transfer
+    @Test
+    public void testMoneyTransfer() {
+    }
+    //Prueba de excepción AccountDoesNotExistException en el método transfer
+    @Test
+    public void testMoneyTransferToAccountThatDoesntExists() {
+    }
+    //Prueba del método transfer
+    @Test
+    public void testMoneyTransferInSameBank() {
+    }
+    //Prueba del método addTimedPayment
+    @Test
+    public void testAddTimedPayment() {
+    }
+    //Prueba del método removeTimedPayment
+    @Test
+    public void testRemoveTimedPayment() {
+    }
+    //Prueba del método tick
+    @Test
+    public void testTick() {
+    }
+    //Prueba de excepción AccountDoesNotExistException en el método tick
+    @Test
+    public void testTickToAccountThatDoesntExist() {
+    }
+}
