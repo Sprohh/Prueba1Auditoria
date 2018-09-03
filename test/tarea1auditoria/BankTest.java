b16903fcbe0c2628ed09d9145e24ebb8648da9fc
Sergio Orellana <sergio.orellana@taisachile.cl>
2018-09-03 18:18:07 -0300

    Se actualiza test de Bank

--- test/tarea1auditoria/BankTest.java
+++ test/tarea1auditoria/BankTest.java
@@ -7,55 +7,159 @@ package tarea1auditoria;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
  * @author orell
  */
 public class BankTest {
+    Currency clpCurrency;
+    Bank bank;
     
     public BankTest() {
     }
+    
+    @Before
+    public void createBankObject()
+    {
+        clpCurrency = new Currency ("CLP", 0.0015);
+        bank = new Bank("BCI", clpCurrency);
+    }
+    
 
     //Prueba del método openAccount
     @Test
     public void testOpenAccount() {
+        try
+        {
+            bank.openAccount("123456789");
+        }
+        catch (AccountExistsException e)
+        {
+            
+        }
     }
     //Prueba de excepción AccountExistsException en el método openAccount
     @Test
     public void testAccountAlreadyExist() {
+        boolean accountAlreadyExists = false;
+        try
+        {
+            bank.openAccount("123456789");
+        }
+        catch (AccountExistsException e)
+        {
+            accountAlreadyExists = true;
+        }
+        
+        assertTrue(accountAlreadyExists);
     }
     //Prueba del método deposit
     @Test
     public void testDeposit() {
+        Money money = new Money(300000, clpCurrency);
+        try
+        {
+            bank.deposit("000", money);
+        }
+        catch (AccountDoesNotExistException e)
+        {
+        }
     }
     //Prueba de excepción AccountDoesNotExistException en el método deposit
     @Test
     public void testDepositToAccountThatDoesntExists() {
+        boolean accountDoesntExists = false;
+        Money money = new Money(300000, clpCurrency);
+        try
+        {
+            bank.deposit("000", money);
+        }
+        catch (AccountDoesNotExistException e)
+        {
+            accountDoesntExists = true;
+        }
+        assertTrue(accountDoesntExists);
     }
     //Prueba del método withdraw
     @Test
     public void testWithdraw() {
+        Money money = new Money(300000, clpCurrency);
+        try
+        {
+            bank.withdraw("000", money);
+        }
+        catch (AccountDoesNotExistException e)
+        {
+        }
     }
     //Prueba de excepción AccountDoesNotExistException en el método withdraw
     @Test
     public void testWithdrawToAccountThatDoesntExists() {
+        boolean accountDoesntExists = false;
+        Money money = new Money(300000, clpCurrency);
+        try
+        {
+            bank.withdraw("000", money);
+        }
+        catch (AccountDoesNotExistException e)
+        {
+            accountDoesntExists = true;
+        }
+        assertTrue(accountDoesntExists);
     }
     //Prueba del método getBalance
     @Test
     public void testGetBalance() {
+        try
+        {
+            bank.getBalance("000");
+        }
+        catch (AccountDoesNotExistException e)
+        {
+        }
     }
     //Prueba de excepción AccountDoesNotExistException en el método getBalance
     @Test
     public void testGetBalanceToAccountThatDoesntExists() {
+        boolean accountDoesntExists = false;
+        try
+        {
+            bank.getBalance("000");
+        }
+        catch (AccountDoesNotExistException e)
+        {
+            accountDoesntExists = true;
+        }
+        assertTrue(accountDoesntExists);
     }
     //Prueba del método transfer
     @Test
     public void testMoneyTransfer() {
+        Money money = new Money(300000, clpCurrency);
+        try
+        {
+            bank.transfer("000","111",money);
+        }
+        catch (AccountDoesNotExistException e)
+        {
+        }
     }
     //Prueba de excepción AccountDoesNotExistException en el método transfer
     @Test
     public void testMoneyTransferToAccountThatDoesntExists() {
+        boolean accountDoesntExists = false;
+        Money money = new Money(300000, clpCurrency);
+        try
+        {
+            bank.transfer("000","111",money);
+        }
+        catch (AccountDoesNotExistException e)
+        {
+            accountDoesntExists = true;
+        }
+        assertTrue(accountDoesntExists);
     }
     //Prueba del método transfer
     @Test
