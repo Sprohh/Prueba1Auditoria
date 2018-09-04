c50fbe4978053e8be5be420acff17659c563b043
DESKTOP-72RTSVI\Brayan <brayan.mendez@taisachile.cl>
2018-09-04 13:26:42 -0300

    Se realizan test de 4 metodos de Money

--- test/tarea1auditoria/MoneyTest.java
+++ test/tarea1auditoria/MoneyTest.java
@@ -18,12 +18,30 @@ public class MoneyTest {
     }
 
     @Test
+    public void testToString()
+    {
+        Currency currency = new Currency ("CLP", 0.0015);
+        Money money = new Money(9999, currency );
+        String result = "99.99 CLP";
+        assertEquals(money.toString(), result);
+    }
+    
+    @Test
+    public void universalValue(){
+        Currency currency = new Currency ("CLP", 0.0015);
+        Money money = new Money(68000, currency );  
+        assertTrue(money.universalValue() == 102);
+    }
+    
+    /*Test para verificar si el monto de dinero es 0, si resulta ser 0 devolvera True */
+    @Test
     public void testIsZeroTrue() {
         Currency currency = new Currency ("CLP", 0.0015);
         Money money = new Money(0, currency );
         assertTrue(money.isZero());
     }
     
+    /*Test para verificar si el monto de dinero es 0, si resulta no ser 0 devolvera False */
     @Test
     public void testIsZeroFalse() {
         Currency currency = new Currency ("CLP", 0.0015);
